55750d26527a4994db9678c0a4cb7f4ae9f2b9b4
Alfonso <adasilva@tetrao.eu>
2024-08-27 10:12:21 +0200

    Edit, show i delete implementats

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,11 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
+        @session('message')
+            <div class="success-message">
+                {{ session('message') }}
+            </div>
+        @endsession
         {{ $slot }}
     </body>
 </html>
